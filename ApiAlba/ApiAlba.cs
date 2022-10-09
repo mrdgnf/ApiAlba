0e7d883cf237b5aae9071f698ae8848d64459377
mrdgnf <mr.d.yarulin@mail.ru>
2022-10-09 10:33:10 +0500

    Последняя версия

---
+++ ApiAlba/ApiAlba.cs
@@ -0,0 +1,92 @@
+﻿
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+public class ApiAlba
+{
+    public static Dictionary<(string, string), int> spentnews = new Dictionary<(string, string), int>();
+    public static IOrderedEnumerable<KeyValuePair<(string,string),int>> FilteringAlgorithm(string job) {
+
+        var News = new List<News>();
+
+        var relevantnews = new Dictionary<(string, string), int>();
+
+        var keyWords = KeyWords(job);
+
+        ApplicationContext db = new ApplicationContext();
+
+        News = db.NEWS.ToList();
+
+        foreach (var news in News)
+        {
+            foreach (var word in news.text.Split().ToList())
+            {
+                foreach (var keywords in keyWords)
+                {
+                    if (word.ToLower().Contains(keywords.Key.Item1.ToLower()))
+                    {
+                        keyWords[keywords.Key]++;
+                    }
+                }
+            }
+            int IndexByRelevant = 0;
+            foreach (var keywords in keyWords)
+            {
+                IndexByRelevant += keywords.Key.Item2 * keywords.Value;
+            }
+            if (!spentnews.ContainsKey((news.name, news.text)))
+                relevantnews.Add((news.name, news.text), IndexByRelevant);
+        }
+        return relevantnews.OrderByDescending(news => news.Value);
+    }
+    
+
+    public static Dictionary<(string, int),int> KeyWords(string job)
+    {
+        Dictionary<(string, int), int> KeyWords = new Dictionary<(string, int), int>();
+        switch (job)
+        {
+            case ("главный директор"):
+                KeyWords.Add(("финан", 3),0);
+                KeyWords.Add(("эконом", 3),0);
+                KeyWords.Add(("санкц", 1),0);
+                KeyWords.Add(("нефть", 1),0);
+                KeyWords.Add(("курс", 3),0);
+                KeyWords.Add(("доллар", 1),0);
+                KeyWords.Add(("евро", 1),0);
+                KeyWords.Add(("банк", 1),0);
+                KeyWords.Add(("ввп", 1),0);
+                KeyWords.Add(("   ", 0),0);
+                break;
+            case ("бухгалтер"):
+                KeyWords.Add(("налог", 5),0);
+                KeyWords.Add(("закон", 8),0);
+                KeyWords.Add(("указ", 5),0);
+                KeyWords.Add(("мрот", 1),0);
+                KeyWords.Add(("путин", 1),0);
+                KeyWords.Add(("      ", 0),0);
+                KeyWords.Add(("       ", 0),0);
+                KeyWords.Add(("        ", 0),0);
+
+                break;
+            default:
+                break;
+        }
+        return KeyWords;
+    }
+    public static void Main()
+     {
+
+        TelegramMethods.Startup();
+        
+    }
+
+}
+
+
+
+
+
+
+
+
---
+++ ApiAlba/TelegramMethods.cs
@@ -0,0 +1,126 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Extensions.Polling;
+using Newtonsoft.Json.Linq;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+using static System.Net.Mime.MediaTypeNames;
+using System.Xml.Linq;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+public class TelegramMethods
+ {
+
+    static ITelegramBotClient bot = new TelegramBotClient("5780617332:AAG540pUZn9qeyCGmOGwP6aZtRewSqkzrek");
+
+    
+    public static void Startup() {
+
+        Console.WriteLine("Запущен бот " + bot.GetMeAsync().Result.FirstName);
+
+        var cts = new CancellationTokenSource();
+        var cancellationToken = cts.Token;
+        var receiverOptions = new ReceiverOptions
+        {
+            AllowedUpdates = { },
+        };
+        bot.StartReceiving(
+            HandleUpdateAsync,
+            HandleErrorAsync,
+            receiverOptions,
+            cancellationToken
+        );
+        Console.ReadLine();
+
+    }
+    public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Telegram.Bot.Types.Update update, CancellationToken cancellationToken)
+    {
+
+        Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject($"{update.Message?.Chat.FirstName} {update.Message?.Chat.LastName} написал: {update.Message?.Text}"));
+
+        if (update.Type == UpdateType.Message)
+        {
+            var message = update.Message;
+            string albertsend;
+            switch (message?.Text?.ToLower())
+            {
+                case (null):
+                    albertsend = "Извините, я не понял что вы написали :(";
+                    break;
+                case ("/start"):
+                    albertsend = "Добро пожаловать, тут находятсья самые актуальные новости!";
+                    break;
+                case ("главный директор"):
+                    GetNewsForTelegramBot("главный директор",botClient,update);
+                    albertsend = null;
+                    break;
+                case ("/digest1"):
+                    GetNewsForTelegramBot("главный директор", botClient, update);
+                    albertsend = null;
+                    break;
+                case ("/digest2"):
+                    GetNewsForTelegramBot("бухгалтер", botClient, update);
+                    albertsend = null; ;
+                    break;
+                case ("бухгалтер"):
+                    GetNewsForTelegramBot("бухгалтер", botClient, update);
+                    albertsend = null; ;
+                    break;
+                default:
+                    albertsend = "/digest1 or /digest2";
+                    break;
+            }
+            if (albertsend != null)
+            {
+                await botClient.SendTextMessageAsync(message.Chat, albertsend);
+            }
+            return;
+        }
+    }
+    public static void GetNewsForTelegramBot(string job,ITelegramBotClient botClient, Telegram.Bot.Types.Update update)
+    {
+
+        var relevantnews = ApiAlba.FilteringAlgorithm(job);
+
+        var message = update.Message;
+
+        int count1 = 0;
+
+        foreach (var news in relevantnews)
+        {
+            count1++;
+        }
+
+        if (count1 <= 0) { botClient.SendTextMessageAsync(message.Chat, "Дайджесты на сегодня кончились, приходите завтра"); ; return; }
+
+        int count2 = 0;
+
+        foreach (var news in relevantnews)
+        {
+            if (count2 >= 3) return;
+            Thread.Sleep(50);
+            botClient.SendTextMessageAsync(message.Chat, $"{news.Key.Item1.ToUpper()}\n{news.Key.Item2}");
+            ApiAlba.spentnews.Add(news.Key, news.Value);
+            count2++;
+        }
+    }
+
+    public static async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+    {
+        Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(exception));
+    }
+
+    public static void Print(string name,string text, Telegram.Bot.Types.Update update, ITelegramBotClient botClient)
+    {
+ 
+        var message = update.Message;
+        botClient.SendTextMessageAsync(message.Chat, $"{name.ToUpper()}\n{text}");
+    }
+
+
+}
+
